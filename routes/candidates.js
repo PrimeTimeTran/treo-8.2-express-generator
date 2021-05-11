1cccbe5dbf5c6f1582e31a5925b6158ced9d2222
Loi Van Tran <datloiboi@gmail.com>
2021-05-11 16:29:41 +0700

    Update with patch/post

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,16 +1,57 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var fs = require("fs");
 
+let candidateDataRaw;
 
-const candidatesData = require('./db.json')
+function getCandidatesData() {
+  fs.readFile(`${__dirname}/db.json`, "utf8", function (err, data) {
+    if (err) throw err;
+    candidateDataRaw = JSON.parse(data);
+  });
+}
+
+getCandidatesData();
+
+function save(data) {
+  const json = JSON.stringify(data);
+  fs.writeFile("./routes/db.json", json, function (err) {
+    if (err) return console.log(err);
+  });
+}
 
 /* GET candidates listing. */
-router.get('/', function(req, res, next) {
-  const candidates = candidatesData.filter(c => c.company === req.query.company);
+router.get("/", function (req, res, next) {
+  let candidates = candidateDataRaw;
+
+  const allowedFilters = ["country", "company", "ethnicity"];
+
+  const queryStringFilters = Object.keys(req.query);
+  if (queryStringFilters.length !== 0) {
+    queryStringFilters.map((filter) => {
+      candidates = candidates.filter((c) => c[filter] === req.query[filter]);
+    });
+  }
   res.json(candidates);
 });
 
+router.post("/", (req, res) => {
+  const candidates = candidateDataRaw;
+  const candidate = req.body;
+  candidate.id = candidates.length + 1;
+  candidates.push(candidate);
+  save(candidates);
+  res.json(candidate);
+});
 
-
+router.patch("/:id", (req, res) => {
+  const candidates = candidateDataRaw;
+  const idx = candidates.findIndex((c) => c.id === parseInt(req.params.id));
+  let candidate = candidates[idx];
+  candidate = { ...candidate, ...req.body };
+  candidates[idx] = candidate;
+  save(candidates);
+  res.json(candidate);
+});
 
 module.exports = router;
